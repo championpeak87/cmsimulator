96a667fb32df2f0f94c2686c02180a608b843638
Kamil Lihan <k.lihan@outlook.com>
2020-02-20 20:11:28 +0100

    NOT TESTED: Added regex multiplicators ({[number],[number]}

--- app/src/main/java/fiitstu/gulis/cmsimulator/elements/RegexTest.java
+++ app/src/main/java/fiitstu/gulis/cmsimulator/elements/RegexTest.java
@@ -20,6 +20,12 @@ public class RegexTest {
     private static final String ONE_OR_MORE = "+";
     private static final String NONE_OR_ONE = "?";
 
+    // multiplicators
+    private static final String MULTIPLICATOR_START = "{";
+    private static final String MULTIPLICATOR_END = "}";
+
+    private static final String LOGICAL_OR = "|";
+
     private static int QUANTIFIERS_DEPTH;
 
     // groups
@@ -61,6 +67,9 @@ public class RegexTest {
             case NONE_OR_MORE:
             case ONE_OR_MORE:
             case NONE_OR_ONE:
+            case MULTIPLICATOR_START:
+            case MULTIPLICATOR_END:
+            case LOGICAL_OR:
                 return true;
         }
         return false;
@@ -76,8 +85,19 @@ public class RegexTest {
             outputTests = handleNoneOrOne(outputTests);
         while (containsOneOrMore(outputTests))
             outputTests = handleOneOrMore(outputTests);
+        while (containsMultiplicator(outputTests))
+            outputTests = handleMultiplicator(outputTests);
 
         return outputTests;
+    }
+
+    private boolean containsMultiplicator(List<String> input) {
+        for (String string : input) {
+            if (string.contains(MULTIPLICATOR_START) && string.contains(MULTIPLICATOR_END))
+                return true;
+        }
+
+        return false;
     }
 
     private boolean containsNoneOrOne(List<String> input) {
@@ -108,6 +128,217 @@ public class RegexTest {
         }
 
         return false;
+    }
+
+    private enum MULTIPLICATOR_TYPE {
+        EXACT_COUNT,
+        MORE_THAN,
+        LESS_THAN,
+        IN_BETWEEN
+    }
+
+    private MULTIPLICATOR_TYPE getMultiplicatorType(String regex, int multiplicator_position) {
+        MULTIPLICATOR_TYPE result = null;
+        boolean hasStartNumber = false;
+        boolean hasEndNumber = false;
+        boolean hasSeparator = false;
+
+        if (regex.charAt(multiplicator_position) == MULTIPLICATOR_START.charAt(0)) {
+            int counter = multiplicator_position + 1;
+            while (regex.charAt(counter) >= '0' && regex.charAt(counter) <= '9') {
+                counter++;
+                hasStartNumber = true;
+            }
+            if (regex.charAt(counter) == ',') {
+                hasSeparator = true;
+                counter++;
+            }
+            while (regex.charAt(counter) >= '0' && regex.charAt(counter) <= '9') {
+                hasEndNumber = true;
+                counter++;
+            }
+            if (regex.charAt(counter) == MULTIPLICATOR_END.charAt(0)) {
+                if (hasStartNumber) {
+                    result = MULTIPLICATOR_TYPE.EXACT_COUNT;
+                } else if (hasStartNumber && hasSeparator) {
+                    result = MULTIPLICATOR_TYPE.MORE_THAN;
+                } else if (hasStartNumber && hasSeparator && hasEndNumber) {
+                    result = MULTIPLICATOR_TYPE.IN_BETWEEN;
+                } else if (hasSeparator && hasEndNumber) {
+                    result = MULTIPLICATOR_TYPE.LESS_THAN;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int getExactMultiplicatorCount(String regex, int multiplicator_position) {
+        int exact_count = 0;
+        int counter = multiplicator_position + 1;
+        while (regex.charAt(counter) >= '0' && regex.charAt(counter) <= '9') {
+            exact_count = exact_count * 10 + Integer.parseInt(String.valueOf(regex.charAt(counter)));
+            counter++;
+        }
+        if (regex.charAt(counter) == MULTIPLICATOR_END.charAt(0))
+            return exact_count;
+        else return -1;
+    }
+
+    private int[] getInBetweenMultiplicatorCount(String regex, int multiplicator_position) {
+        int max_count = 0;
+        int min_count = 0;
+        int counter = multiplicator_position + 1;
+
+        while (regex.charAt(counter) >= '0' && regex.charAt(counter) <= '9') {
+            min_count = min_count * 10 + Integer.parseInt(String.valueOf(regex.charAt(counter)));
+            counter++;
+        }
+
+        if (regex.charAt(counter) == ',')
+            counter++;
+        else return null;
+
+        while (regex.charAt(counter) >= '0' && regex.charAt(counter) <= '9') {
+            max_count = max_count * 10 + Integer.parseInt(String.valueOf(regex.charAt(counter)));
+            counter++;
+        }
+        if (regex.charAt(counter) == MULTIPLICATOR_END.charAt(0)) {
+            int result[] = new int[2];
+            result[0] = min_count;
+            result[1] = max_count;
+            return result;
+        } else return null;
+    }
+
+    private int getMaxMultiplicatorCount(String regex, int multiplicator_position) {
+        int max_count = 0;
+        int counter = multiplicator_position + 1;
+        if (regex.charAt(counter) == ',')
+            counter++;
+        else return -1;
+        while (regex.charAt(counter) >= '0' && regex.charAt(counter) <= '9') {
+            max_count = max_count * 10 + Integer.parseInt(String.valueOf(regex.charAt(counter)));
+            counter++;
+        }
+        if (regex.charAt(counter) == MULTIPLICATOR_END.charAt(0))
+            return max_count;
+        else return -1;
+    }
+
+    private int getMinMultiplicatorCount(String regex, int multiplicator_position) {
+        int min_count = 0;
+        int counter = multiplicator_position + 1;
+        while (regex.charAt(counter) >= '0' && regex.charAt(counter) <= '9') {
+            min_count = min_count * 10 + Integer.parseInt(String.valueOf(regex.charAt(counter)));
+            counter++;
+        }
+        if (regex.charAt(counter) == ',')
+            counter++;
+        if (regex.charAt(counter) == MULTIPLICATOR_END.charAt(0))
+            return min_count;
+        else return -1;
+    }
+
+    private List<String> handleMultiplicator(List<String> input) {
+        final char CHAR_MULTIPLICATOR_START = MULTIPLICATOR_START.charAt(0);
+        final char CHAR_MULTIPLICATOR_END = MULTIPLICATOR_END.charAt(0);
+        List<String> regex_strings = new ArrayList<String>();
+
+        for (String _input : input) {
+            char[] input_char = _input.toCharArray();
+            int input_char_size = _input.length();
+
+            for (int i = 0; i < input_char_size; i++) {
+                if (input_char[i] == CHAR_MULTIPLICATOR_START) {
+
+                    String replacement_string = null;
+                    String replication_pattern = null;
+
+                    MULTIPLICATOR_TYPE type = getMultiplicatorType(_input, i);
+                    int min_repeat_count = -1;
+                    int max_repeat_count = -1;
+                    int exact_repeat_count = -1;
+                    switch (type) {
+                        case EXACT_COUNT:
+                            exact_repeat_count = getExactMultiplicatorCount(_input, i);
+                            break;
+                        case MORE_THAN:
+                            min_repeat_count = getMinMultiplicatorCount(_input, i);
+                            break;
+                        case LESS_THAN:
+                            max_repeat_count = getMaxMultiplicatorCount(_input, i);
+                            break;
+                        case IN_BETWEEN:
+                            final int[] in_between_array = getInBetweenMultiplicatorCount(_input, i);
+                            min_repeat_count = in_between_array[0];
+                            max_repeat_count = in_between_array[1];
+                            break;
+                    }
+                    boolean contains_group = false;
+                    if (i == 0) {
+                        replacement_string = NONE_OR_MORE;
+                        replication_pattern = EMPTY_SYMBOL;
+
+                        regex_strings.add(_input.replace(replacement_string, replication_pattern));
+                    } else {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        if (input_char[i - 1] == GROUP_END.charAt(0)) {
+                            contains_group = true;
+                            for (int j = i - 2; j >= 0; j--) {
+                                if (input_char[j] == GROUP_START.charAt(0)) {
+                                    replication_pattern = _input.subSequence(j + 1, i - 1).toString();
+                                    stringBuilder.append(GROUP_START);
+                                    stringBuilder.append(replication_pattern);
+                                    stringBuilder.append(GROUP_END);
+                                    break;
+                                }
+                            }
+                        }
+                        if (!contains_group)
+                            stringBuilder.append(input_char[i - 1]);
+                        stringBuilder.append(NONE_OR_MORE);
+
+                        if (!contains_group) {
+                            replication_pattern = String.valueOf(input_char[i - 1]);
+                        }
+                        replacement_string = stringBuilder.toString();
+
+                        String replacementString;
+                        switch (type) {
+                            case EXACT_COUNT:
+                                replacementString = repeatString(replication_pattern, exact_repeat_count);
+                                regex_strings.add(_input.replaceFirst(Pattern.quote(replacement_string), replacementString));
+                                break;
+                            case MORE_THAN:
+                                for (int j = min_repeat_count; j < QUANTIFIERS_DEPTH; j++) {
+                                    replacementString = repeatString(replication_pattern, j);
+                                    regex_strings.add(_input.replaceFirst(Pattern.quote(replacement_string), replacementString));
+                                }
+                                break;
+                            case LESS_THAN:
+                                for (int j = 1; j < max_repeat_count; j++)
+                                {
+                                    replacementString = repeatString(replication_pattern, j);
+                                    regex_strings.add(_input.replaceFirst(Pattern.quote(replacement_string), replacementString));
+                                }
+                                break;
+                            case IN_BETWEEN:
+                                for (int j = min_repeat_count; j < max_repeat_count; j++)
+                                {
+                                    replacementString = repeatString(replication_pattern, j);
+                                    regex_strings.add(_input.replaceFirst(Pattern.quote(replacement_string), replacementString));
+                                }
+                                break;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return regex_strings;
     }
 
     private List<String> handleNoneOrMore(List<String> input) {
